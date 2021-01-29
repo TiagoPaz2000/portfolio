aade1f7a6dc2c25e910b7ebc0502235d076d5a75
Tiago de Almeida Paz <tiagopaz.dev@gmail.com>
2021-01-29 14:04:12 -0300

    melhorando sintaxes

--- Projetos/contadorDeDinheiro/script.js
+++ Projetos/contadorDeDinheiro/script.js
@@ -2,6 +2,7 @@ const values = document.querySelectorAll('.values');
 const result1 = document.querySelector('#result1');
 const result2 = document.querySelector('#result2');
 const result3 = document.querySelector('#result3');
+const clearBtn = document.querySelector('#clearButton');
 
 function teste() {
   let array = [];
@@ -14,31 +15,24 @@ function teste() {
     }
     return element;
   });
-  console.log(teste2);
   return teste2;
 }
 
 function calc() {
   const array = [0.05, 0.10, 0.25, 0.50, 1.00, 2.00, 5.00, 10.00, 20.00, 50.00, 100.00, 200.00];
-  let valueMoedas = 0;
-  let valueNotas = 0;
+  let valueMoedas = 0, valueNotas = 0;
   teste().forEach((element, index) => {
     if (index < 5) {
       valueMoedas += element * array[index];
-    }
-    if (index >= 5) {
+    } else {
       valueNotas += element * array[index];
     }
   })
-
-  let valueTotal = valueMoedas + valueNotas;
-
   result1.innerHTML = 'Resultado de Moedas: ' + valueMoedas.toFixed(2);;
   result2.innerHTML = 'Resultado de Notas: ' + valueNotas;
-  result3.innerHTML = 'Resultado Total: ' + valueTotal.toFixed(2);
+  result3.innerHTML = 'Resultado Total: ' + (valueMoedas + valueNotas).toFixed(2);
 }
 
-const clearBtn = document.querySelector('#clearButton');
 clearBtn.addEventListener('click', () => {
   result1.innerHTML = 'Resultado de Moedas: ';
   result2.innerHTML = 'Resultado de Notas: ';
